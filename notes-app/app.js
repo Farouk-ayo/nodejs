ccd9d79c1db806c4d3ebdc260bd024228708c892
Farouk-ayo <mustaphafarouk41@gmail.com>
2024-05-13 10:33:48 +0100

    removed nodemodules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,19 @@
-const fs = require("fs");
+// const fs = require("fs");
 const add = require("./utils.js");
+const validator = require("validator");
+const chalk = require("chalk");
+
+console.log(validator.isEmail("example.com"));
+console.log(chalk.red.inverse.bold("Error"));
+
+console.log(validator.isURL("https://ayo.com"));
+const getNotes = require("./note.js");
+
+const msg = getNotes();
+console.log(msg);
 
 const sum = add(3, 5);
 console.log(sum);
 
-fs.writeFileSync("notes.txt", "This file was created by No dejs ");
-fs.fs.appendFileSync("notes.txt", "  i lIVE IN nigeria");
+// fs.writeFileSync("notes.txt", "This file was created by No dejs ");
+// fs.appendFileSync("notes.txt", "  i lIVE IN nigeria");
