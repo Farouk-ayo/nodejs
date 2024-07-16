30b67cfff60dbfeaac4512bdc598b8c1989f04d7
Farouk-ayo <mustaphafarouk41@gmail.com>
2024-07-16 18:21:45 +0100

    parsed body

--- node-1/http.js
+++ node-1/http.js
@@ -1,13 +1,48 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req.method, req.url, req.headers);
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    // res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title> My First Page</title></head>");
+
+    // the name of the input here must be noted , because it will come out in the parsed body
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</body>");
+    res.write("</html>");
+
+    // use return so that the rest wont run
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+  //   console.log(req.method, req.url, req.headers);
   //   process.exit();
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title> My First Page</title></head>");
   res.write("<body><h1>Hello from my Node js server</h1></body>");
   res.write("</body>");
+  res.write("</html>");
   res.end();
 });
 
