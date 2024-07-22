d8d58c007804fd1ea8c59bbe4720207a13794a0d
Farouk-ayo <mustaphafarouk41@gmail.com>
2024-07-22 17:00:23 +0100

    html pages in views

--- expressjs/app.js
+++ expressjs/app.js
@@ -8,9 +8,12 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
-app.use(adminRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send("<h1> Page not Found</h1>");
+});
 // const server = http.createServer(app);
 // server.listen("3000");
 // or 👇
--- expressjs/routes/admin.js
+++ expressjs/routes/admin.js
@@ -2,15 +2,17 @@ const express = require("express");
 
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
   //   next(); // allows the request to continue to the next middlewate in the line
   // if you don't call next, the request will stop here
   res.send(
-    '<form action="/product" method="POST" ><input type="text" name="title"><button type="submit">Add Product</button></form>'
+    '<form action="/admin/add- product" method="POST" ><input type="text" name="title"><button type="submit">Add Product</button></form>'
   );
 });
 
-router.post("/product", (req, res, next) => {
+// /admin/add-product => POST
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
