3a89be44b71953f100c8565a300a4e0c06314428
Farouk-ayo <mustaphafarouk41@gmail.com>
2024-08-05 11:01:38 +0100

    pug lesson

--- expressjs/routes/admin.js
+++ expressjs/routes/admin.js
@@ -1,21 +1,34 @@
 const express = require("express");
+
 // check the helper function in the util for the rootDir
-const rootDir = require("../util/path");
+
+// const rootDir = require("../util/path");
+
 const path = require("path");
+const { render } = require("pug");
 
 const router = express.Router();
+
+const products = [];
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
   //   next(); // allows the request to continue to the next middlewate in the line
   // if you don't call next, the request will stop here
-  res.sendFile(path.join(rootDir, "../", "views", "add-product.html"));
+  // res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+  });
 });
 
 // /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
   console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+// module.exports = router;
+exports.routes = router;
+exports.products = products;
--- expressjs/routes/shop.js
+++ expressjs/routes/shop.js
@@ -3,9 +3,16 @@ const path = require("path");
 const express = require("express");
 
 const router = express.Router();
+const adminData = require("./admin");
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  console.log(adminData.products);
+  // res.sendFile(path.join(__dirname, "../", "views", "shop.html"))
+  res.render("shop", {
+    prods: adminData.products,
+    pageTitle: "Shop",
+    path: "/",
+  });
 });
 
 module.exports = router;
