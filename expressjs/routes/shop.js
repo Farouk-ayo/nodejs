f032dec5b64df19d84498510164cf01878a74ba6
Farouk-ayo <mustaphafarouk41@gmail.com>
2024-09-16 12:10:42 +0100

    refractoring views et contrls

--- expressjs/routes/shop.js
+++ expressjs/routes/shop.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const productController = require("../controllers/products");
-router.get("/", productController.getProducts);
+const shopController = require("../controllers/shop");
+router.get("/", shopController.getIndex);
+
+router.get("/products", shopController.getProducts);
+router.get("/cart", shopController.getCart);
+router.get("/checkout", shopController.getCheckout);
 
 module.exports = router;
