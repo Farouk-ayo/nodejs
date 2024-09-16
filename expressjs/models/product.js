0ac80bd76a9e0a2ffa6a585f421d8e03b4881b4c
Farouk-ayo <mustaphafarouk41@gmail.com>
2024-09-16 11:14:09 +0100

    refractory

--- expressjs/models/product.js
+++ expressjs/models/product.js
@@ -1,6 +1,19 @@
-const products = [];
 const path = require("path");
 const fs = require("fs");
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
+
+const getProductFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return cb([]);
+    }
+    return cb(JSON.parse(fileContent));
+  });
+};
 
 module.exports = class Product {
   constructor(t) {
@@ -8,23 +21,14 @@ module.exports = class Product {
   }
 
   save() {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
       });
     });
   }
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    getProductFromFile(cb);
   }
 };
