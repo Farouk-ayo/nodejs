28b6bbb22eea94dbd1d2660d4ce9012bb15893da
Farouk-ayo <mustaphafarouk41@gmail.com>
2024-09-16 13:08:47 +0100

    edit et add

--- expressjs/controllers/admin.js
+++ expressjs/controllers/admin.js
@@ -23,7 +23,7 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
-    res.render("admin/products ", {
+    res.render("admin/products", {
       prods: products,
       pageTitle: "Admin Product",
       path: "/admin/products",
