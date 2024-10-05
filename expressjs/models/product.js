0eafc79d3735aa110cf81728f11c2a2ecd9dbca0
Farouk-ayo <mustaphafarouk41@gmail.com>
2024-10-05 16:35:26 +0100

    done

--- expressjs/models/product.js
+++ expressjs/models/product.js
@@ -5,6 +5,7 @@ const p = path.join(
   "data",
   "products.json"
 );
+const Cart = require("./cart");
 
 const getProductFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
@@ -16,7 +17,8 @@ const getProductFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -24,12 +26,23 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
     getProductFromFile((products) => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
-      });
+      if (this.id) {
+        const existingProductIndex = products.findIndex(
+          (prod) => prod.id === this.id
+        );
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+      }
     });
   }
   static fetchAll(cb) {
@@ -39,6 +52,17 @@ module.exports = class Product {
     getProductFromFile((products) => {
       const product = products.find((p) => p.id === id);
       cb(product);
+    });
+  }
+  static deleteById(id) {
+    getProductFromFile((products) => {
+      const product = products.find((p) => p.id === id);
+      const updatedProducts = products.filter((p) => p.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+        if (!err) {
+          Cart.deleteProduct(id, product.price);
+        }
+      });
     });
   }
 };
--- expressjs/routes/shop.js
+++ expressjs/routes/shop.js
@@ -7,6 +7,8 @@ router.get("/products", shopController.getProducts);
 router.get("/products/:productId", shopController.getProduct);
 router.get("/cart", shopController.getCart);
 
+router.post("/cart-delete-item", shopController.postCartDeleteProduct);
+
 router.post("/cart", shopController.postCart);
 router.get("/orders", shopController.getOrders);
 router.get("/checkout", shopController.getCheckout);
