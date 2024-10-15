4c17d39bf6bd90febce83cc663192f453c44cc94
Farouk-ayo <mustaphafarouk41@gmail.com>
2024-10-15 13:01:50 +0100

    done

--- expressjs/util/database.js
+++ expressjs/util/database.js
@@ -1,8 +1,29 @@
-const { Sequelize } = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("node-1", "root", "Faroukayoo@24", {
-  dialect: "mysql",
-  host: "localhost",
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://Faroukayo:Faroukayo@cluster0.2tlgmgj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+    .then((client) => {
+      console.log("connected");
+      _db = client.db("shop");
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No databse found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
