7a5c52948788d43a1de5aa3f5c15a78fc0ff2192
Farouk-ayo <mustaphafarouk41@gmail.com>
2024-10-23 14:05:04 +0100

    sessions et cookies

--- expressjs/app.js
+++ expressjs/app.js
@@ -1,20 +1,41 @@
 const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
+const MONGODB_URL =
+  "mongodb+srv://Faroukayo:Faroukayo@cluster0.2tlgmgj.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0";
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URL,
+  collection: "sessions",
+  // expires
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
 
 app.use((req, res, next) => {
   User.findById("6716d7f19cefde8a07d3bc07")
@@ -26,14 +47,13 @@ app.use((req, res, next) => {
 });
 
 app.use("/admin", adminRoutes);
-
 app.use(shopRoutes);
+app.use(authRoutes);
+
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    "mongodb+srv://Faroukayo:Faroukayo@cluster0.2tlgmgj.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
-  )
+  .connect(MONGODB_URL)
   .then((result) => {
     User.findOne().then((user) => {
       if (!user) {
@@ -47,7 +67,8 @@ mongoose
         user.save();
       }
     });
-
     app.listen(3000);
   })
-  .catch((err) => console.log(err));
+  .catch((err) => {
+    console.log(err);
+  });
