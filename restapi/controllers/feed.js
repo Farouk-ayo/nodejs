706156b3251f2fdbdbcd3ea7c206753399297d7b
Farouk-ayo <mustaphafarouk41@gmail.com>
2025-04-05 23:49:14 +0100

    feat: initialize REST API with basic structure and routes
    - Created package.json for project dependencies and scripts
    - Added express and body-parser as dependencies
    - Set up nodemon for development
    - Implemented feed routes for getting and creating posts

---
+++ restapi/controllers/feed.js
@@ -0,0 +1,25 @@
+exports.getPosts = (req, res) => {
+  //   res.status(200).json({
+  //     message: "Posts fetched successfully",
+  //   });
+
+  res.status(200).json({
+    posts: [
+      {
+        title: "First Post",
+        content: "This is the first post",
+      },
+    ],
+  });
+};
+
+exports.createPost = (req, res) => {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  // Dummy response for now
+  res.status(201).json({
+    message: "Post created successfully",
+    post: { id: new Date().toISOString(), title, content },
+  });
+};
